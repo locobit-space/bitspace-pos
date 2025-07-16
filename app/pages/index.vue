ca7c5b8565d431e64507644aeecf1ecc382da040
Mark AI <chanthavong_12@hotmail.com>
2025-07-16 19:41:55 +0700

    add structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,18 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import Crypto from "crypto-js";
-onMounted(() => {
-  const text = "hello worldxxx";
-  const hash = Crypto.HmacSHA1(text, "secret key").toString();
-  const encrypted = Crypto.AES.encrypt(text, "secret key").toString();
-  console.log({ encrypted, hash });
 
-  const decrypted = Crypto.AES.decrypt(encrypted, "secret key").toString(Crypto.enc.Utf8);
-  const hash2 = Crypto.HmacSHA1(decrypted, "secret key").toString();
-  console.log({ decrypted });
-
-});
 </script>
 
 <style scoped></style>
